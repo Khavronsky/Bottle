69c79c127e3c4ffb2ab82d340e1b9a846274d668
Khavronsky <Khavronsky@gmail.com>
2017-01-15 19:29:32 +0300

    prepare to coding

---
+++ app/src/main/java/com/khavronsky/bottle/AdapterToBaseFragment.java
@@ -0,0 +1,22 @@
+package com.khavronsky.bottle;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class AdapterToBaseFragment extends FragmentPagerAdapter {
+
+    public AdapterToBaseFragment(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}

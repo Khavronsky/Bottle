9d338c847627fc18cb573fe58ac6df618b6b4bbb
khavronsky <khavronsky@gmail.com>
2017-01-16 13:38:59 +0300

    DataModel is done.

--- app/src/main/java/com/khavronsky/bottle/DataModel.java
+++ app/src/main/java/com/khavronsky/bottle/DataModel.java
@@ -1,13 +1,43 @@
 package com.khavronsky.bottle;
 
 
-public class DataModel {
+class DataModel {
 
-        CapacityType pics;
-        String title;
-        int value;
-        int step;
+    private CapacityType pics;
+    private String title;
+    private int value;
+    private int step;
 
+    public CapacityType getPics() {
+        return pics;
+    }
 
+    public void setPics(CapacityType pics) {
+        this.pics = pics;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
 }
 

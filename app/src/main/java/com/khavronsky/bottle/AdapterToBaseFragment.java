cc9602cf0f47d7960e8c4228b9f7fc48c36b2b1e
khavronsky <khavronsky@gmail.com>
2017-01-16 15:04:16 +0300

    Adapter is done.

--- app/src/main/java/com/khavronsky/bottle/AdapterToBaseFragment.java
+++ app/src/main/java/com/khavronsky/bottle/AdapterToBaseFragment.java
@@ -1,22 +1,45 @@
 package com.khavronsky.bottle;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class AdapterToBaseFragment extends FragmentPagerAdapter {
+import java.util.List;
 
-    public AdapterToBaseFragment(FragmentManager fm) {
+public class AdapterToBaseFragment extends FragmentPagerAdapter {
+    private int fragmentCount;
+    private CapacityType[] pics;
+    private String[] title;
+    private int[] value;
+    private int[] capacityStep;
+
+
+    public AdapterToBaseFragment(FragmentManager fm, List<DataModel> dataModels) {
         super(fm);
+        fragmentCount = dataModels.size();
+        for (int i = 0; i < fragmentCount; i++) {
+            pics[i] = dataModels.get(i).getPics();
+            title[i] = dataModels.get(i).getTitle();
+            value[i] = dataModels.get(i).getValue();
+            capacityStep[i] = dataModels.get(i).getCapacityStep();
+        }
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Bundle bundle = new Bundle();
+        bundle.putString(BasedFragment.TITLE, title[position]);
+        bundle.putInt(BasedFragment.VALUE, value[position]);
+        bundle.putInt(BasedFragment.CAPACITY_STEP, capacityStep[position]);
+        bundle.putSerializable(BasedFragment.PICS, pics[position]);
+        Fragment fragment = new BasedFragment();
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return fragmentCount;
     }
 }

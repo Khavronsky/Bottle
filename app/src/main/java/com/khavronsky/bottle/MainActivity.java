faf9205caf49b61d2f5d71798731fab87e7c00d8
Khavronsky <Khavronsky@gmail.com>
2017-01-18 19:07:14 +0300

    add callback to plus or minus buttonpressed

--- app/src/main/java/com/khavronsky/bottle/MainActivity.java
+++ app/src/main/java/com/khavronsky/bottle/MainActivity.java
@@ -1,16 +1,40 @@
 package com.khavronsky.bottle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton plusButton;
+    ImageButton minusButton;
+    CirclesSlideIndicator slideIndicator;
+    TextView titleCapacity;
+    ChangeCapacityCallback capacityCallback;
+    int currentCapacityID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
 
 
+
+    }
+
+    private void init() {
+        plusButton = (ImageButton) findViewById(R.id.button_plus);
+        minusButton = (ImageButton) findViewById(R.id.button_minus);
+        titleCapacity = (TextView) findViewById(R.id.tv_title_of_capacity);
+        slideIndicator = (CirclesSlideIndicator) findViewById(R.id.circlesSlideIndicator);
+    }
+
+    interface ChangeCapacityCallback{
+        void buttonPlusOrMinusPressed (int dataModelID, boolean buttonPressed);
+    }
+    void subscribeCCCallback (ChangeCapacityCallback callback){
+        this.capacityCallback = callback;
     }
 }

f0f1692062e41fb769359de975d6da3e93219a17
khavronsky <khavronsky@gmail.com>
2017-01-19 17:13:53 +0300

    WaterWindow is working

--- app/src/main/java/com/khavronsky/bottle/BaseFragment.java
+++ app/src/main/java/com/khavronsky/bottle/BaseFragment.java
@@ -7,49 +7,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class BaseFragment extends Fragment {
     static final String PICS = "pics";
-    static final String TITLE = "title";
-    static final String VALUE = "value";
-    static final String CAPACITY_STEP = "capacityStep";
 
-    ImageButton plusButton;
-    ImageButton minusButton;
     ImageView imageView;
-    CirclesSlideIndicator slideIndicator;
-    TextView titleCapacity;
-
+    CapacityType capacityType=CapacityType.BOTTLE;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_main, container, false);
-        plusButton = (ImageButton) view.findViewById(R.id.button_plus);
-        minusButton = (ImageButton) view.findViewById(R.id.button_minus);
+        View view = inflater.inflate(R.layout.base_fragment, container, false);
         imageView = (ImageView) view.findViewById(R.id.picture_capacity);
-        titleCapacity = (TextView) view.findViewById(R.id.tv_title_of_capacity);
-        slideIndicator = (CirclesSlideIndicator) view.findViewById(R.id.circlesSlideIndicator);
-
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            CapacityType pic = (CapacityType) bundle.getSerializable(PICS);
-            if (pic != null) {
-                switch (pic) {
-                    case BOTTLE:
-                        imageView.setImageResource(R.drawable.water_botle_full);
-                        return view;
-                    case GLASS:
-                        imageView.setImageResource(R.drawable.water_glass_full);
-                        return view;
-                    case DROP:
-                        imageView.setImageResource(R.drawable.water_drop_full);
-                        return view;
-                }
-            }
-        }
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setCapacityTypeImage(capacityType);
+    }
+
+    public void setCapacityType(CapacityType capacityType)
+
+    {
+        this.capacityType=capacityType;
+    }
+    public void setCapacityTypeImage(CapacityType capacityType)
+
+    {
+        switch (capacityType) {
+            case BOTTLE:
+                imageView.setImageResource(R.drawable.water_botle_full);
+                break;
+            case GLASS:
+                imageView.setImageResource(R.drawable.water_glass_full);
+                break;
+            case DROP:
+                imageView.setImageResource(R.drawable.water_drop_full);
+                break;
+        }
+
+
     }
 }
--- app/src/main/java/com/khavronsky/bottle/MainActivity.java
+++ app/src/main/java/com/khavronsky/bottle/MainActivity.java
@@ -1,26 +1,60 @@
 package com.khavronsky.bottle;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     ImageButton plusButton;
     ImageButton minusButton;
     CirclesSlideIndicator slideIndicator;
     TextView titleCapacity;
-    ChangeCapacityCallback capacityCallback;
+    TextView valueOfCapacity;
+    ButtonListener buttonListener;
+
     int currentCapacityID;
+    int currentScreen;
+    int currentCapacityValueOnScreen;
+    List<DataModel> dataModelList;
+    AdapterToBaseFragment adapterToBaseFragment;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ClassForLightTesting testing = new ClassForLightTesting(this);
+        dataModelList = testing.getList();
+
         init();
+        setViewParamFromList(dataModelList.get(currentScreen));
 
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentScreen = position;
+                currentCapacityID = dataModelList.get(position).getId();
+                currentCapacityValueOnScreen = dataModelList.get(position).getCapacityStep();
+                setViewParamFromList(dataModelList.get(position));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
 
@@ -28,13 +62,47 @@ public class MainActivity extends AppCompatActivity {
         plusButton = (ImageButton) findViewById(R.id.button_plus);
         minusButton = (ImageButton) findViewById(R.id.button_minus);
         titleCapacity = (TextView) findViewById(R.id.tv_title_of_capacity);
+        valueOfCapacity = (TextView) findViewById(R.id.tv_value_of_capacity);
         slideIndicator = (CirclesSlideIndicator) findViewById(R.id.circlesSlideIndicator);
+        slideIndicator.setCountOfCircle(dataModelList.size());
+        currentCapacityID = dataModelList.get(0).getId();
+        currentCapacityValueOnScreen = dataModelList.get(0).getCapacityStep();
+        viewPager = (ViewPager) findViewById(R.id.my_pager);
+        adapterToBaseFragment = new AdapterToBaseFragment(getSupportFragmentManager(), dataModelList);
+        viewPager.setAdapter(adapterToBaseFragment);
+        plusButton.setOnClickListener(this);
+        minusButton.setOnClickListener(this);
+
     }
 
-    interface ChangeCapacityCallback{
-        void buttonPlusOrMinusPressed (int dataModelID, boolean buttonPressed);
+    private void setViewParamFromList(DataModel dataModel) {
+
+        titleCapacity.setText(dataModel.getTitle());
+        valueOfCapacity.setText(String.valueOf(dataModel.getCapacityStep()));
+        slideIndicator.setFocusedCircle(currentScreen);
+        slideIndicator.invalidate();
+
     }
-    void subscribeCCCallback (ChangeCapacityCallback callback){
-        this.capacityCallback = callback;
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.button_plus:
+                Toast.makeText(this, "PLUS PRESSED", Toast.LENGTH_SHORT).show();
+                buttonListener.buttonPlusOrMinusPressed(currentCapacityID, true);
+                break;
+            case R.id.button_minus:
+                Toast.makeText(this, "MINUS PRESSED", Toast.LENGTH_SHORT).show();
+                buttonListener.buttonPlusOrMinusPressed(currentCapacityID, false);
+                break;
+        }
+    }
+
+    interface ButtonListener {
+        void buttonPlusOrMinusPressed(int dataModelID, boolean buttonPressed);
+    }
+    void setOnButtonPlusMinusListener(ButtonListener buttonListener){
+        this.buttonListener = buttonListener;
     }
 }

3e6b499505aaafa89cba3b25541bbff4d36ce40c
khavronsky <khavronsky@gmail.com>
2017-01-19 17:11:42 +0300

    del castom callback and add default callback

--- app/src/main/java/com/khavronsky/bottle/AdapterToBaseFragment.java
+++ app/src/main/java/com/khavronsky/bottle/AdapterToBaseFragment.java
@@ -11,34 +11,24 @@ import java.util.List;
 public class AdapterToBaseFragment extends FragmentPagerAdapter {
     private List<DataModel> dataModels = new ArrayList<>();
 
-    interface VPCallback {
-        void vpCallback(int id);
-    }
-
-    private VPCallback callback;
-
-    void subscribeCallBack(VPCallback callback){
-        this.callback = callback;
-    }
-
     public AdapterToBaseFragment(FragmentManager fm, List<DataModel> dataModels) {
         super(fm);
         this.dataModels = dataModels;
     }
 
-
     @Override
     public Fragment getItem(int position) {
         Bundle bundle = new Bundle();
         bundle.putSerializable(BaseFragment.PICS, dataModels.get(position).getPics());
-        Fragment fragment = new BaseFragment();
+        BaseFragment fragment = new BaseFragment();
         fragment.setArguments(bundle);
-        callback.vpCallback(dataModels.get(position).getId());
+        fragment.setCapacityType( dataModels.get(position).getPics());
+//        callback.vpCallback(dataModels.get(position).getId());
         return fragment;
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return dataModels.size();
     }
 }

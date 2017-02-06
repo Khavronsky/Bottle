7e31ea26adbe1c4cd65aef01585ac1bd3b5a2eca
khavronsky <khavronsky@gmail.com>
2017-02-06 19:13:32 +0300

    add picker, refactoring & bugfix

--- app/src/main/java/com/khavronsky/bottle/MyCustomViews/NewWaterCapacity.java
+++ app/src/main/java/com/khavronsky/bottle/MyCustomViews/NewWaterCapacity.java
@@ -2,16 +2,21 @@ package com.khavronsky.bottle.MyCustomViews;
 
 
 import android.content.Context;
-import android.support.v4.app.FragmentActivity;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.khavronsky.bottle.Adapters.AdapterToWaterPicsOnViewPagerFragment;
+import com.khavronsky.bottle.Data.DataForWaterScreen;
 import com.khavronsky.bottle.Data.DataModelToAddWaterView;
 import com.khavronsky.bottle.R;
 
@@ -26,8 +31,16 @@ public class NewWaterCapacity extends CardView implements View.OnClickListener {
     TextView buttonSave;
     EditText inputTitle;
     ViewPager viewPager;
+    NumberPicker numberPicker;
+    CirclesSlideIndicator slideIndicator;
+    private int currentCapacityID;
+    FragmentManager childFragmentManager;
+
+    DataForWaterScreen data;
+
     private List<DataModelToAddWaterView> dataModelToAddWaterViewList = new ArrayList<>();
     AdapterToWaterPicsOnViewPagerFragment adapter;
+    private int currentScreen;
 
     public NewWaterCapacity(Context context) {
         super(context);
@@ -47,6 +60,19 @@ public class NewWaterCapacity extends CardView implements View.OnClickListener {
         init();
     }
 
+    public void setData(DataForWaterScreen data) {
+        this.data = data;
+        this.dataModelToAddWaterViewList = data.getDataModelToAddWaterViews();
+        firstSetView();
+    }
+
+    public void setChildFragmentManager(FragmentManager childFragmentManager){
+        if(viewPager != null){
+            this.childFragmentManager = childFragmentManager;
+        }
+        setAdapter();
+    }
+
     private void init() {
         LayoutInflater inflater = LayoutInflater.from(getContext());
         inflater.inflate(R.layout.new_water_capacity, this);
@@ -56,12 +82,84 @@ public class NewWaterCapacity extends CardView implements View.OnClickListener {
         buttonCancel = (TextView) findViewById(R.id.button_cancel);
         buttonSave = (TextView) findViewById(R.id.button_save);
         inputTitle = (EditText) findViewById(R.id.edit_capacity_title);
+        slideIndicator = (CirclesSlideIndicator) findViewById(R.id.circlesSlideIndicator);
         viewPager = (ViewPager) findViewById(R.id.my_pager);
-        adapter = new AdapterToWaterPicsOnViewPagerFragment(((FragmentActivity) context).getSupportFragmentManager(), dataModelToAddWaterViewList);
+        numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+
+    }
+
+    private void firstSetView(){
+        buttonCancel.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+        buttonDel.setOnClickListener(this);
+        slideIndicator.setCountOfCircle(dataModelToAddWaterViewList.size());
+        slideIndicator.setFocusedCircle(currentScreen);
+        slideIndicator.invalidate();
+        currentCapacityID = dataModelToAddWaterViewList.get(0).getId();
+        if (childFragmentManager != null){
+            setAdapter();
+        }
+        setPicker();
+        setListenerToViewPager();
+    }
+
+    private void setListenerToViewPager() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                currentScreen = position;
+                Log.d("MyX", "NewWaterCapacity pos" + position);
+                currentCapacityID = dataModelToAddWaterViewList.get(position).getId();
+                slideIndicator.setFocusedCircle(currentScreen);
+                slideIndicator.invalidate();
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private void setPicker() {
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(40);
+        numberPicker.setDisplayedValues(displayedValuesFormat());
+        numberPicker.setWrapSelectorWheel(false);
+    }
+
+    @NonNull
+    private String[] displayedValuesFormat() {
+        String[] capacityValues = new String[41];
+        for (int i = 0; i < capacityValues.length; i++) {
+            int x = 20 + i;
+            String number = Integer.toString(x*50);
+            capacityValues[i] = number;
+        }
+        return capacityValues;
+    }
+
+    private void setAdapter() {
+        adapter = new AdapterToWaterPicsOnViewPagerFragment(childFragmentManager, dataModelToAddWaterViewList);
+        viewPager.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View v) {
-
+        int id = v.getId();
+        switch (id){
+            case R.id.button_cancel:
+                (Toast.makeText(getContext(), "cancel", Toast.LENGTH_SHORT)).show();
+                break;
+            case R.id.button_save:
+                (Toast.makeText(getContext(), "save", Toast.LENGTH_SHORT)).show();
+                break;
+            case R.id.button_del:
+                (Toast.makeText(getContext(), "delete", Toast.LENGTH_SHORT)).show();
+                break;
+        }
     }
 }

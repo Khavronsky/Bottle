b7e173d3d2875e95329f82a0a74dd5028ec1a0f3
khavronsky <khavronsky@gmail.com>
2017-02-06 19:12:05 +0300

    add toolbar & menu to choosing fragments

--- app/src/main/java/com/khavronsky/bottle/Activityes/MainActivity.java
+++ app/src/main/java/com/khavronsky/bottle/Activityes/MainActivity.java
@@ -1,32 +1,65 @@
 package com.khavronsky.bottle.Activityes;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.khavronsky.bottle.Fragments.NewWaterCapacityFragment;
 import com.khavronsky.bottle.Fragments.WaterScreenFragment;
 import com.khavronsky.bottle.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startFragment();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        if (getSupportActionBar() != null) {
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//            getSupportActionBar().setHomeButtonEnabled(true);
+//        }
     }
 
-    public void startFragment() {
-
+    public void startFragment(Fragment fragment) {
         try {
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction()
-                    .replace(R.id.activity_main, new WaterScreenFragment())
+                    .replace(R.id.activity_main, fragment)
                     .addToBackStack(null)
                     .commit();
         } catch (Exception e) {
             e.printStackTrace();
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case (R.id.water_consumed):
+                startFragment(new WaterScreenFragment());
+                Log.d("MyLog", "startFragment");
+                break;
+            case (R.id.new_capacity):
+                startFragment(new NewWaterCapacityFragment());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
